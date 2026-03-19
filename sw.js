0238920a0de1b3a608894e06f58a6ca13d4b9036
tasneemm05 <tasneem5.mousa@gmail.com>
2026-03-19 15:34:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "my-recipes-cache-v1";
+const FILES_TO_CACHE = [
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/images/icon.png",
+  "/images/icon-512.png"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate service worker and clean old caches
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Intercept requests and serve cached files
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+  );
+});
